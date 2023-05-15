288470c5f6f82bdee526b9f57a119793b89d21ac
Lullaby330 <69coder@gmail.com>
2023-05-15 11:37:29 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6292019982:AAGcDtVf6iAXj4qYT9TXy6b8PCEj5dLFgE0")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6116458644:AAG17zRP1aS55f3YM1tSIRRJqav3XE9VrIQ")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "6216349"))
@@ -21,8 +21,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1289438071"))
 DB_URI = os.environ.get("DATABASE_URL", "postgres://sendydewals:agW9OTr3FPlE@ep-rapid-violet-017057.us-east-2.aws.neon.tech/Datasql")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001218103395"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001602189104"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001942028586"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001621487705"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
